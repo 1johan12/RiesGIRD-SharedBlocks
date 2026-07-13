48817da7701f19239fcd0f4a66d6719b1cfc6969
jpumahuillca-esan <jpumahuillca@esan.edu.pe>
2026-07-13 10:56:55 -0500

    Update API and storage URLs to production endpoints

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,2 +1,2 @@
-export const PUBLIC_API_URL = 'http://localhost:4000/api/public';
-export const PUBLIC_STORAGE_URL = 'http://127.0.0.1:8000/storage';
+export const PUBLIC_API_URL = 'https://api.redgirdaccperu.edu.pe/api/public';
+export const PUBLIC_STORAGE_URL = 'https://admin.redgirdaccperu.edu.pe/storage';
